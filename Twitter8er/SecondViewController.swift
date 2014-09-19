26d0e3fd57690edade4cae393abc6ba34a941921
Josh Brown <josh@roadfiresoftware.com>
2014-09-19 11:14:33 -0400

    Initial Commit

---
+++ Twitter8er/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Twitter8er
+//
+//  Created by Josh Brown on 9/19/14.
+//  Copyright (c) 2014 Roadfire Software. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
