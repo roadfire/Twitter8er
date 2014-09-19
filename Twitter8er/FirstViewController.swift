af21d6fa7bf7ede3a849f99fc0564499f205f8b0
Josh Brown <josh@roadfiresoftware.com>
2014-09-19 17:01:49 -0400

    initial commit

--- Twitter8er/FirstViewController.swift
+++ Twitter8er/FirstViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as TweetTableViewCell
+        
+        return cell
+    }
 }
 
---
+++ Twitter8er/TweetTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter8er
+//
+//  Created by Josh Brown on 9/19/14.
+//  Copyright (c) 2014 Roadfire Software. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    
+}
